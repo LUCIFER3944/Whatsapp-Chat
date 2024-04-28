930b9161e6e896b93e205dd988c1c7e1e7235223
Lucifer <vivekrai3944@gmail.com>
2024-04-28 12:32:39 +0530

    update

--- script.js
+++ script.js
@@ -62,21 +62,21 @@ document.querySelectorAll('.conversation-back').forEach(function(item) {
 // end: Coversation
 
 // <----------------------------------Conversation js----------------------------->
-let conversation2 = document.querySelector("#conversation-2");
-let conversation3 = document.querySelector("#conversation3");
+// let conversation2 = document.querySelector("#conversation-2");
+// let conversation3 = document.querySelector("#conversation3");
 
-conversation3.addEventListener("click", () => {
+// conversation3.addEventListener("click", () => {
 
-    document.conversation2.innerHTML += conversation2.innerHTML;
+//     document.conversation2.innerHTML += conversation2.innerHTML;
     
-    let newPhotoUrl1 = "image/img (10).jpg";
-    let conversation3Photo = document.querySelector("#conversation3img");
-    let conversation4Photo = document.querySelector("#conversation4img");
+//     let newPhotoUrl1 = "image/img (10).jpg";
+//     let conversation3Photo = document.querySelector("#conversation3img");
+//     let conversation4Photo = document.querySelector("#conversation4img");
     
-    conversation3Photo.src = newPhotoUrl1;
-    conversation4Photo.src = newPhotoUrl1;
+//     conversation3Photo.src = newPhotoUrl1;
+//     conversation4Photo.src = newPhotoUrl1;
   
-});
+// });
 //Don't know it's does not work as it should be so we should just leave it like that lol 
 
 
@@ -96,3 +96,7 @@ $(document).ready(function(){
         });
     });
 });
+
+
+
+                                                        
